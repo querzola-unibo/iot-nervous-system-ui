eb9a839c9b784611d9173ea09c4acdbea8a1b2c4
Emanuele Querzola <emanuele@wonderflow.ai>
2022-12-26 18:24:06 +0100

    feat(ins): init project

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { BrowserRouter } from 'react-router-dom'
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      Ciao
+    </BrowserRouter>
+  )
+}
